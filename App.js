528a08237972ccf17ad28f54f565f57a07018fcb
Reiclid <reiclid@gmail.com>
2025-04-04 23:05:31 +0300

    add module: instal request module and add App.js and navigation

--- App.js
+++ App.js
@@ -1,20 +1,39 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import HomeScreen from "./screens/HomeScreen";
+import TasksScreen from "./screens/TasksScreen";
+
+const initialTasks = [
+  { id: '1', description: 'Зробити 10 кліків', completed: false },
+  { id: '2', description: 'Зробити 5 подвійних кліків', completed: false },
+  { id: '3', description: 'Утримувати об\'єкт 3 секунди', completed: false },
+  { id: '4', description: 'Перетягнути об\'єкт', completed: false },
+  { id: '5', description: 'Зробити свайп вправо', completed: false },
+  { id: '6', description: 'Зробити свайп вліво', completed: false },
+  { id: '7', description: 'Змінити розмір об\'єкта', completed: false },
+  { id: '8', description: 'Отримати 100 очок', completed: false },
+];
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{ title: "Головний екран" }}
+          initialParams={{ tasks: initialTasks }}
+        />
+        <Stack.Screen
+          name="Tasks"
+          component={TasksScreen}
+          options={{ title: "Завдання" }}
+          initialParams={{ initialTasks }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
