98a985a1cc07a1e62ab7dcd804baa279e61d2f6c
Reiclid <reiclid@gmail.com>
2025-05-14 17:59:23 +0300

    create project

--- App.js
+++ App.js
@@ -1,20 +1,39 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect } from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import * as FileSystem from 'expo-file-system';
+
+import FileBrowserScreen from './screens/FileBrowserScreen';
+import FileScreen from './screens/FileScreen';
+
+const Stack = createStackNavigator();
+const APP_DIR = FileSystem.documentDirectory + 'AppData/';
 
 export default function App() {
+  // Створюємо папку AppData, якщо нема
+  useEffect(() => {
+    (async () => {
+      const info = await FileSystem.getInfoAsync(APP_DIR);
+      if (!info.exists) {
+        await FileSystem.makeDirectoryAsync(APP_DIR, { intermediates: true });
+      }
+    })();
+  }, []);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Browser">
+        <Stack.Screen
+          name="Browser"
+          component={FileBrowserScreen}
+          options={{ title: '📂 Файловий менеджер' }}
+        />
+        <Stack.Screen
+          name="File"
+          component={FileScreen}
+          options={({ route }) => ({ title: route.params.name })}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
